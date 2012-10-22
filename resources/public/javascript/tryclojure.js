b0cc01ac25eba90f5d54f8462466f893b3c4da64
Robert Sanders <robert@zeevex.com>
2012-10-21 22:07:45 -0400

    make console taller

--- resources/public/javascript/tryclojure.js
+++ resources/public/javascript/tryclojure.js
@@ -166,8 +166,8 @@ var controller;
 
 $(document).ready(function() {
     controller = $("#console").console({
-        welcomeMessage:'Enter some Clojure code to be evaluated.',
-        promptLabel: 'Clojure> ',
+        welcomeMessage:'Enter a Datalog query to be evaluated.',
+        promptLabel: 'Datalog> ',
         commandValidate: onValidate,
         commandHandle: onHandle,
         autofocus:true,
