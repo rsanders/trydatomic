c02ef318e0a1ea35fb3ef81daf1c380d001771ae
Robert Sanders <robert@zeevex.com>
2012-10-21 22:07:17 -0400

    add datomic dep and sample files

--- project.clj
+++ project.clj
@@ -2,7 +2,10 @@
   :description "A simple web-based Clojure REPL for trying out Clojure without having to install it."
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [noir "1.3.0-beta10"]
-		 [commons-lang/commons-lang "2.5"]
+                 [commons-lang/commons-lang "2.5"]
+                 [com.datomic/datomic-free "0.8.3551"
+                   :exclusions [org.slf4j/slf4j-nop org.slf4j/slf4j-log4j12]]
+                 [ch.qos.logback/logback-classic "1.0.1"]
                  [clojail "1.0.1"]]
   :jvm-opts ["-Djava.security.policy=example.policy""-Xmx80M"]
   :main tryclojure.server)
