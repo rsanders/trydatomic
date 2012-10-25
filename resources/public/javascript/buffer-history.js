a008da0e1d9775e72195d8e095c895d8a53c382b
Robert Sanders <robert@zeevex.com>
2012-10-24 21:28:25 -0400

    define String.trim if not defined

--- resources/public/javascript/buffer-history.js
+++ resources/public/javascript/buffer-history.js
@@ -1,4 +1,10 @@
 
+if(!String.prototype.trim) {  
+  String.prototype.trim = function () {  
+    return this.replace(/^\s+|\s+$/g,'');  
+  };  
+}
+
 function ReplEditor(e) {
   this.editor = e;
   this.replHist = [];
