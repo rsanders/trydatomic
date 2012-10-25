7278d21d6a9952280baa2adcc4b2ab426a2c057f
Robert Sanders <robert@zeevex.com>
2012-10-24 22:29:12 -0400

    upgrade some dependencies

--- README.md
+++ README.md
@@ -3,13 +3,34 @@
 TryDatomic is an online Datomic REPL based on the excellent TryClojure project by 
 Raynes et al.
 
-TryClojure was written using Noir and Chris Done's jquery console (you're awesome, Chris).
+TryClojure was written using Noir and Chris Done's jquery console.
 
 ## Usage
 
-http://tryclj.com
+  http://trydatomic.herokuapp.com
 
 If you want to run it, refer to https://github.com/Raynes/tryclojure/wiki/Running-Tryclojure
+
+## References
+
+See:
+
+  http://docs.datomic.com
+  https://github.com/Datomic/day-of-datomic
+  https://gist.github.com/stuarthalloway
+
+Advanced:
+
+  https://groups.google.com/forum/#!topic/datomic/fKKz_tg9Bos
+
+## Included Sample Data
+
+  http://trydatomic.herokuapp.com/samples/seattle/seattle-schema.dtm
+  http://trydatomic.herokuapp.com/samples/seattle/seattle-data0.dtm
+
+Not automatically loaded:
+
+  http://trydatomic.herokuapp.com/samples/seattle/seattle-data1.dtm
 
 ## Credits
 
--- project.clj
+++ project.clj
@@ -3,13 +3,26 @@
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [noir "1.3.0-beta10"]
                  [commons-lang/commons-lang "2.5"]
-                 [com.datomic/datomic-free "0.8.3551"
+                 [com.datomic/datomic-free "0.8.3561"
                    :exclusions [org.slf4j/slf4j-nop org.slf4j/slf4j-log4j12]]
-                 [ch.qos.logback/logback-classic "1.0.1"]
-                 [org.clojure/tools.nrepl "0.2.0-beta10"]
+                 [ch.qos.logback/logback-classic "1.0.7"]
                  ;; [com.cemerick/drawbridge "0.0.6"]
-                 ;; [ringmon "0.1.2"]
-                 [clojail "1.0.1"]]
+                 [clojail "1.0.3"]]
+  :dev-dependencies [[org.clojure/tools.nrepl "0.2.0-beta10"]]
+
+  ;; see https://github.com/emezeske/lein-cljsbuild/blob/0.2.9/sample.project.clj
+  ;; :plugins [[lein-cljsbuild "0.2.9"]]
+  ;; :hooks [leiningen.cljsbuild]
+  ;; :cljsbuild {
+  ;;   :builds [{
+  ;;       ; The path to the top-level ClojureScript source directory:
+  ;;       :source-path "src-cljs"
+  ;;       ; The standard ClojureScript compiler options:
+  ;;       ; (See the ClojureScript compiler documentation for details.)
+  ;;       :compiler {
+  ;;         :output-to "resources/public/javascript/main.js"  ; default: main.js in current directory
+  ;;         :optimizations :whitespace
+  ;;         :pretty-print true}}]}
   :min-lein-version "2"
   :url "http://trydatomic.herokuapp.com/"
   :jvm-opts ["-Djava.security.policy=example.policy""-Xmx80M"]
